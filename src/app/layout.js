08f216e6248d2dba3da61ed73e559cdd940e0fc7
ericwang0516 <s12740011@go.thu.edu.tw>
2025-12-25 01:27:09 +0800

    v3.8 webicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,16 +4,18 @@ import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 import SmoothScrollProvider from '../components/providers/SmoothScrollProvider';
 import Loader from '../components/ui/Loader';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
-  title: 'IDWeek',
-  description: 'IDWeek Project',
+  title: '2025 東海工設週 | THU IDWEEK',
+  description: '《 2025 東海工設週 THU IDWEEK 》官方網站 | ⌏❬ 孵化 ❭ ⌌ * 孕育 * 校驗 * 重生 | 工業設計、展場規劃、互動藝術...',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="zh-TW">
       <body>
+        <Analytics/>
         <SmoothScrollProvider>
           <Suspense fallback={null}>
             <Loader />
