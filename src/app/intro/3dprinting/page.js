213bd041cbdaeb0efeeb0e3e479a067dadb51e5d
ericwang0516 <s12740011@go.thu.edu.tw>
2025-12-24 17:59:06 +0800

    v3.1 Commm

--- src/app/intro/3dprinting/page.js
+++ src/app/intro/3dprinting/page.js
@@ -3,10 +3,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ddd from './3dprinting.module.css';
 import Image from 'next/image';
+import textData from './texts.json';
 
 export default function ThreeDPrintingPage() {
   const [isIntroVisible, setIsIntroVisible] = useState(false);
   const [isOneVisible, setIsOneVisible] = useState(false);
+  const [randomTexts, setRandomTexts] = useState([]);
   const introRef = useRef(null);
   const oneRef = useRef(null);
 
@@ -36,12 +38,21 @@ export default function ThreeDPrintingPage() {
     return () => observer.disconnect();
   }, []);
 
+  useEffect(() => {
+    if (textData && textData.length > 0) {
+      // Shuffle array and pick first 2 (or more)
+      const shuffled = [...textData].sort(() => 0.5 - Math.random());
+      const selected = shuffled.slice(0, 10); // Pick top 5 for usage
+      setRandomTexts(selected);
+    }
+  }, []);
+
   return (
     <div className={ddd.container}>
       {/* COVER Space */}
       <div className={ddd.cover_container}>
         <div className={ddd.cover_image_container}>
-          <Image src="/images/intro_3d/cover/3dprinter.png" alt="3D Printing Cover" fill objectFit='cover' className={ddd.sway_animation}/>
+          <Image src="/images/intro_3d/cover/3dprinter.png" alt="3D Printing Cover" fill style={{ objectFit: 'cover' }} className={ddd.sway_animation}/>
         </div>
         {/* COVER - TITLE Space */}
         <div className={ddd.cover_title_container}>
@@ -56,7 +67,10 @@ export default function ThreeDPrintingPage() {
         </div>
         {/* COVER - TITLE Space End*/}
         <div className={ddd.cover__late_image_container}>
-          <Image src="/images/intro_3d/cover/3dline.png" alt="3D Printing Cover" fill objectFit='cover'/>
+          <Image src="/images/intro_3d/cover/3dline.png" alt="3D Printing Cover" fill style={{ objectFit: 'cover' }}/>
+        </div>
+        <div className={ddd.cover__late_image_container}>
+          <Image src="/images/intro_3d/cover/3dline.png" alt="3D Printing Cover" fill style={{ objectFit: 'cover' }}/>
         </div>
       </div>
       {/* COVER Space End*/}
@@ -78,20 +92,101 @@ export default function ThreeDPrintingPage() {
       <div className={ddd.content_one_container}>
         <div className={ddd.hor_line} style={{ top: '140px', width: '100%', left:0 }}></div>
         <div className={ddd.hor_line} style={{ top: '315px', width: '40%', right:0 }}></div>
-        <div className={ddd.hor_line} style={{ top: '465px', width: '40%', right:0 }}></div>
-        <div className={ddd.hor_line} style={{ top: '515px', width: '40%', right:0 }}></div>
+        <div className={ddd.hor_line} style={{ top: '465px', width: '80%', right:0 }}></div>
+        <div className={ddd.hor_line} style={{ top: '515px', width: '80%', right:0 }}></div>
+        <div className={ddd.ver_line} style={{ top: '200px', height: '180px', right:'200px' }}></div>
+
+        <div className={ddd.hor_line} style={{ top: '815px', width: '80%', right:0 }}></div>
+        <div className={ddd.hor_line} style={{ top: '715px', width: '90%', right:0 }}></div>
+        <div className={ddd.ver_line} style={{ top: '715px', height: '180px', right:'353px' }}></div>
+        <div className={ddd.ver_line} style={{ top: '200px', height: '180px', right:'200px' }}></div>
+        
         <div className={ddd.content_one_box1_image_container}>
-          <Image src="/images/intro_3d/content/one/fusion.png" alt="Mod" fill objectFit='cover'/>
+          <Image src="/images/intro_3d/content/one/fusion.png" alt="Mod" fill style={{ objectFit: 'cover' }}/>
         </div>
-        <div className={ddd.content_one_box1_title_container}>
-          <div className={ddd.content_one_box1_title_container_number}>01</div>
-          <div className={ddd.content_one_box1_title_container_box}>
-            <p className={ddd.content_one_box1_title}>建模</p>
-            <p className={ddd.content_one_box1_title_sub}>使用建模軟體(如 : blender、fusion、rhino等)建出模型，轉存STL檔案即可送印。</p>
+        <div className={ddd.content_title_container} style={{ top: '40px', right: '50px' }}>
+          <div className={ddd.content_title_container_number}>01</div>
+          <div className={ddd.content_title_container_box}>
+            <p className={ddd.content_title}>建模</p>
+            <p className={ddd.content_title_sub}>使用建模軟體 (如 : blender、fusion、rhino等) 建出模型，轉存STL檔案即可送印。</p>
           </div>
+        </div>
+        <div className={ddd.paper_container} style={{ top: '300px', left: '-60px' }}>
+          <Image src="/images/paper/paper1.png" alt="Paper" fill style={{ objectFit: 'cover' }}/>
+          <div className={ddd.paper_container_text} style={{ rotate: '8deg' }}>{randomTexts[0]}</div>
+        </div>
+        <div className={ddd.content_one_box2_image_container}>
+          <Image src="/images/intro_3d/content/one/image_card1.png" alt="Mod" fill style={{ objectFit: 'cover' }}/>
+        </div>
+        <div className={ddd.paper_container} style={{ top: '720px', left: '10px', width: '280px', height: '230px', zIndex: 2}}>
+          <Image src="/images/paper/paper2.png" alt="Paper" fill style={{ objectFit: 'cover' }}/>
+          <div className={ddd.paper_container_text} style={{ rotate: '12deg' }}>{randomTexts[1]}</div>
         </div>
       </div>
       {/* CONTENT One Space End*/}
+      {/* CONTENT Two Space */}
+      <div className={ddd.content_two_container}>
+        <div className={ddd.hor_line} style={{ top: '140px', width: '100%', left:0 }}></div>
+        <div className={ddd.hor_line} style={{ top: '315px', width: '40%', right:0 }}></div>
+        <div className={ddd.hor_line} style={{ top: '465px', width: '80%', right:0 }}></div>
+        <div className={ddd.hor_line} style={{ top: '515px', width: '80%', right:0 }}></div>
+        <div className={ddd.ver_line} style={{ top: '100px', height: '180px', right:'200px' }}></div>
+        <div className={ddd.ver_line} style={{ top: '500px', height: '180px', right:'200px' }}></div>
+        <div className={ddd.content_two_box1_image_container}>
+          <Image src="/images/intro_3d/content/two/sh.png" alt="Mod" fill style={{ objectFit: 'cover' }}/>
+        </div>
+        <div className={ddd.content_title_container} style={{ top: '40px', left: '50px' }}>
+          <div className={ddd.content_title_container_number}>02</div>
+          <div className={ddd.content_title_container_box} style={{ height: '240px' }}>
+            <p className={ddd.content_title}>切片</p>
+            <p className={ddd.content_title_sub}>將3D的建模轉換為2D的語言讓機器讀取的過程。因為機器列印時是圖層疊起的方式列印，可以將切片想像成將3D切成2D圖層的過程。</p>
+          </div>
+        </div>
+        <div className={ddd.paper_container} style={{ top: '400px', left: '-60px', width: '280px', height: '230px' , rotate: '-20deg' }}>
+          <Image src="/images/paper/paper2.png" alt="Paper" fill style={{ objectFit: 'cover' }}/>
+          <div className={ddd.paper_container_text} style={{ rotate: '12deg' }}>{randomTexts[2]}</div>
+        </div>
+        <div className={ddd.paper_container} style={{ top: '620px', right: '50px', width: '280px', height: '230px', zIndex: 2 }}>
+          <Image src="/images/paper/paper3.png" alt="Paper" fill style={{ objectFit: 'cover' }}/>
+          <div className={ddd.paper_container_text} style={{ rotate: '13deg' }}>{randomTexts[3]}</div>
+        </div>
+      </div>
+      {/* CONTENT Two Space End*/}
+      {/* CONTENT Three Space */}
+      <div className={ddd.content_three_container}>
+        <div className={ddd.hor_line} style={{ top: '140px', width: '100%', left:0 }}></div>
+        <div className={ddd.hor_line} style={{ top: '315px', width: '40%', right:0 }}></div>
+        <div className={ddd.hor_line} style={{ top: '515px', width: '100%', right:0 }}></div>
+        <div className={ddd.ver_line} style={{ top: '100px', height: '180px', right:'200px' }}></div>
+        <div className={ddd.ver_line} style={{ top: '515px', height: '480px', right:'200px' }}></div>
+        <div className={ddd.hor_line} style={{ top: '1000px', width: '100%', right:0 }}></div>
+        <div className={ddd.content_three_box1_image_container}>
+          <Image src="/images/intro_3d/content/three/printing.png" alt="Mod" fill style={{ objectFit: 'cover' }}/>
+        </div>
+        <div className={ddd.content_title_container} style={{ top: '40px', right: '50px' }}>
+          <div className={ddd.content_title_container_number}>03</div>
+          <div className={ddd.content_title_container_box} style={{ height: '270px' }}>
+            <p className={ddd.content_title}>列印</p>
+            <p className={ddd.content_title_sub}>根據列印​的​材質​不同，​成型​原理、​精細度、​成本、​材料​和​後處理​也​不同，​這邊​簡單​以​FD​M​與光固化​舉例。​FDM​是​將線​材融化從​噴嘴​擠出​成型，​精細​度​較低、​後續​處理​較麻煩，​但​成本​較低。​</p>
+          </div>
+        </div>
+        <div className={ddd.content_three_txt}>
+          光固化​全​名​光敏樹脂固化，​是​使用​紫外​線​光源​照射液​態​樹脂，​使其​逐層固化​的​列印​方式。​精細​程度​高，​表面​平滑，​後續​處裡較​簡單，​但​成本​較高。​
+        </div>
+        <div className={ddd.paper_container} style={{ top: '400px', right: '-60px', width: '280px', height: '230px' , rotate: '-20deg' }}>
+          <Image src="/images/paper/paper1.png" alt="Paper" fill style={{ objectFit: 'cover' }}/>
+          <div className={ddd.paper_container_text} style={{ rotate: '12deg' }}>{randomTexts[4]}</div>
+        </div>
+        <div className={ddd.paper_container} style={{ top: '860px', right: '10px', width: '280px', height: '230px' }}>
+          <Image src="/images/paper/paper3.png" alt="Paper" fill style={{ objectFit: 'cover' }}/>
+          <div className={ddd.paper_container_text} style={{ rotate: '12deg' }}>{randomTexts[5]}</div>
+        </div>
+        <div className={ddd.paper_container} style={{ top: '600px', left: '-60px', width: '280px', height: '280px' , rotate: '-20deg' }}>
+          <Image src="/images/paper/paper4.png" alt="Paper" fill style={{ objectFit: 'cover' }}/>
+          <div className={ddd.paper_container_text} style={{ rotate: '-20deg' }}>{randomTexts[6]}</div>
+        </div>
+      </div>
+      {/* CONTENT Three Space End*/}
     </div>
   );
-}
+};
---
+++ src/app/intro/spray/page.js
@@ -0,0 +1,111 @@
+import styles from './spray.module.css';
+
+export default function Page() {
+  return (
+    <main className={styles.v49_2}>
+    <span className={styles.v51_21}>關於噴漆</span>
+    <span className={styles.v51_22}>製作模型時上色的步驟，除了「噴」我們還會...</span>
+    <div className={styles.v51_23} />
+    <div className={styles.v51_24} />
+    <div className={styles.v58_37} />
+    <div className={styles.v58_38} />
+    <div className={styles.v58_39} />
+    <div className={styles.v108_15} />
+    <div className={styles.v69_4} />
+    <span className={styles.v58_40}>01</span>
+    <div className={styles.v58_41}>
+      <span className={styles.v58_42}>打磨</span>
+      <span className={styles.v58_43}>為​了​使​模型​的​表面​更​光滑、​質感​更​好，​會​使用​砂紙將列​印好​的​磨型​打磨，​不同​的​材質​列印打磨​所​花費時間​與力氣​也​不同。​</span>
+    </div>
+    <span className={styles.v62_4}>在亮眼之前，<br />得先經過漫長的磨練</span>
+    <span className={styles.v62_3}>當模型的形體成形後，還需要一段漫長的「打磨」旅程。<br /><br />砂磨、補土與噴漆組成了三個最不起眼、卻最費工的步驟。<br /><br />正是這些反覆的細節累積，讓模型真正變得完整。</span>
+    <div className={styles.v62_13} />
+    <div className={styles.v62_12} />
+    <div className={styles.v62_26} />
+    <div className={styles.v62_27} />
+    <div className={styles.v108_17} />
+    <div className={styles.v69_2} />
+    <div className={styles.v62_28} />
+    <div className={styles.v62_29} />
+    <div className={styles.v107_5} />
+    <div className={styles.v107_6} />
+    <div className={styles.v62_30} />
+    <div className={styles.v107_8} />
+    <div className={styles.v109_24} />
+    <div className={styles.v109_25} />
+    <span className={styles.v62_31}>02</span>
+    <div className={styles.v62_33} />
+    <span className={styles.v62_34}>03</span>
+    <div className={styles.v62_35} />
+    <div className={styles.v109_26} />
+    <div className={styles.v109_19} />
+    <div className={styles.v109_20} />
+    <div className={styles.v109_22} />
+    <div className={styles.v62_36} />
+    <div className={styles.v62_37}>
+      <span className={styles.v62_38}>補土</span>
+      <span className={styles.v62_39}>補起​表面​上​的​小洞​使​質感​更​好，​常用​的​有​汽車​補土與蝴蝶​補土。​汽車補​土用​於微小​的​瑕疵，​不​用​加入​硬化劑，​但​會溶解​噴漆，​所以​一定​要​在​噴漆​前​使用​! ​蝴蝶補土​需要​硬化劑，​但​可以​補範​圍​較大​的​瑕疵。​</span>
+    </div>
+    <div className={styles.v62_40} />
+    <div className={styles.v62_41}>
+      <span className={styles.v62_42}>噴漆</span>
+      <span className={styles.v62_43}>一定​ !​ 絕對 ! ​要​少量​多​次​噴 ​! 否則​你​積漆流​的​眼淚​會變成​你​流​的​眼淚​</span>
+    </div>
+    <div className={styles.v62_46} />
+    <div className={styles.v109_27} />
+    <span className={styles.v108_12}>如果​是​用​FDM、​PLA​等​材質，​建議​從​150號​砂紙​打磨​至​600~800號砂紙；​若模型​使用​光固化​列印，​可以​從400號砂紙開始磨​至​800號</span>
+    <span className={styles.v109_18}>當​噴漆​上​模型​一眼​就​能​看​出模型​在​先前​有​沒​有​被​細緻​處理(​打磨​與​補土​的​步驟​)​ 別抱著僥倖心態​想​用​噴漆​補​起​小​凹洞，​積漆​後​要​從​打磨​開始重新​處裡…</span>
+    <div className={styles.v114_159}>
+      <span className={styles.v64_95}>© thuidweek 2025</span>
+      <span className={styles.v64_88}>HOME</span>
+      <div className={styles.v109_31}>
+        <span className={styles.v109_29}>About</span>
+        <span className={styles.v64_91}>GOODS</span>
+        <span className={styles.v64_92}>DESIGNERS</span>
+      </div>
+      <div className={styles.v109_30}>
+        <span className={styles.v109_28}>Introduction</span>
+        <span className={styles.v64_90}>3D PRINT</span>
+        <span className={styles.v64_89}>SPRAY</span>
+      </div>
+      <div className={styles.v109_32} />
+      <div className={styles.v109_33} />
+      <div className={styles.v109_34} />
+      <div className={styles.v110_43} />
+    </div>
+    <div className={styles.v340_75} />
+    <div className={styles.v340_76} />
+    <div className={styles.v340_79} />
+    <div className={styles.v340_77} />
+    <div className={styles.v340_78} />
+    <div className={styles.v387_270} />
+    <div className={styles.v395_337} />
+    <span className={styles.v395_338}>變身<br />人體吸塵器</span>
+    <div className={styles.v395_341} />
+    <div className={styles.v408_97} />
+    <span className={styles.v395_343}>補完補土後都會獲得半永久美甲</span>
+    <span className={styles.v395_336}>這個模型有點難磨 <br />ㄉㄟˇ男模</span>
+    <div className={styles.v395_349} />
+    <div className={styles.v408_99} />
+    <span className={styles.v395_350}>補土很臭真的很臭</span>
+    <div className={styles.v395_352} />
+    <span className={styles.v395_353}>您好，<br />我大吸一口</span>
+    <span className={styles.v408_95}>噴完都會叫模型漂亮寶貝，也會稱讚同學的模型是<br />別人家孩子</span>
+    <span className={styles.v408_96}>哇靠臭到哭 <br />噴頭還對到自己<br />🤡</span>
+    <span className={styles.v408_100}>磨完之後<br />手比工人還粗</span>
+    <div className={styles.v408_111} />
+    <span className={styles.v408_112}>這個模型有<br />點難磨 <br />ㄉㄟˇ男模</span>
+    <div className={styles.v410_116} />
+    <div className={styles.v410_165}>
+      <div className={styles.v54_26}>
+        <div className={styles.v49_5} />
+      </div>
+      <div className={styles.v312_228} />
+      <div className={styles.v312_229} />
+      <span className={styles.v340_163}>IDWeek</span>
+    </div>
+    <span className={styles.v410_167}>我不磨了 w(ﾟДﾟ)w 我不磨了</span>
+    <div className={styles.v49_3} />
+    </main>
+  );
+}
